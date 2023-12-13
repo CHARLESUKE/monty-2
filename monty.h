df653e593c3d08e2570f83684545b1c2a6b59780
CHARLESUKE <ukcharleay@gmail.com>
2023-12-13 01:55:25 -0800

    monty2

--- monty.h
+++ monty.h
@@ -14,13 +14,17 @@
 #include <ctype.h>
 
 /**
-* struct stack_s - doubly linked list representation of a stack (or queue)
-* @n: integer
-* @prev: points to the previous element of the stack (or queue)
-* @next: points to the next element of the stack (or queue)
+* struct stack_s - this is the doubly linked list
+* representing or representation of a stack (or queue)
+* @n: this variable is the integer
+* @prev: this variable points 2 d previous element
+* of d stack 'or the queue'
+* @next: this variable is points 2 d next
+* element of d stack (or the queue)
 *
-* Description: doubly linked list node structure
-* for stack, queues, LIFO, FIFO
+* Description: this is the doubly linked list
+* node structure for
+* stack, queues, LIFO, FIFO
 */
 typedef struct stack_s
 {
@@ -31,12 +35,12 @@ typedef struct stack_s
 
 
 /**
-* struct instruction_s - opcode and its function
-* @opcode: the opcode
-* @f: function to handle the opcode
+* struct instruction_s - this is the opcode and its function
+* @opcode: this variable is the opcode
+* @f: this variable function is to handle the opcode
 *
-* Description: opcode and its function
-* for stack, queues, LIFO, FIFO
+* Description: this is the opcode and its
+* function for stack, queues, LIFO, FIFO
 */
 typedef struct instruction_s
 {
@@ -45,13 +49,14 @@ typedef struct instruction_s
 } instruction_t;
 
 /**
-* struct func_t -structure including arguments, contents of file
-* file and flag
-* @arg: value
-* @file: monty file
-* @content: file content
-* @fl: file flag
-* Description: carries values for each monty function
+* struct func_t -structure including arguments
+* contents of file file and flag
+* @arg: this variable is the  value
+* @file: this variable is the monty file
+* @content: this variable is th file content
+* @fl: this variable is te file flag
+* Description: the work is to carries values for each monty
+* function
 */
 typedef struct func_t
 {
@@ -67,47 +72,47 @@ void push_function(stack_t **headboy, unsigned int countline);
 void pall_function(stack_t **headboy, unsigned int countline);
 
 /*Task 1*/
-void monty_pint(stack_t **head, unsigned int line_counter);
+void monty_pint(stack_t **headboy, unsigned int countline);
 
 /*Task 2*/
-void monty_pop(stack_t **head, unsigned int line_counter);
+void monty_pop(stack_t **headboy, unsigned int countline);
 
 /*Task 3*/
-void monty_swap(stack_t **head, unsigned int line_counter);
+void monty_swap(stack_t **headboy, unsigned int countline);
 
 /*Task 4*/
-void monty_add(stack_t **head, unsigned int line_counter);
+void monty_add(stack_t **headboy, unsigned int countline);
 
 /*Task 5*/
-void monty_nop(stack_t **head, unsigned int line_counter);
+void monty_nop(stack_t **headboy, unsigned int countline);
 
 /*Task 6*/
-void monty_sub(stack_t **head, unsigned int line_counter);
+void monty_sub(stack_t **headboy, unsigned int countline);
 
 /*Task 7*/
-void monty_div(stack_t **head, unsigned int line_counter);
+void monty_div(stack_t **headboy, unsigned int countline);
 
 /*Task 8*/
-void monty_mul(stack_t **head, unsigned int line_counter);
+void monty_mul(stack_t **headboy, unsigned int countline);
 
 /*Task 9*/
-void monty_mod(stack_t **head, unsigned int line_counter);
+void monty_mod(stack_t **headboy, unsigned int countline);
 
 /*Task 11*/
-void monty_pchar(stack_t **head, unsigned int line_count);
+void monty_pchar(stack_t **headboy, unsigned int countline);
 
 /*Task 12*/
-void monty_pstr(stack_t **head, unsigned int line_count);
+void monty_pstr(stack_t **headboy, unsigned int countline);
 
 /*Task 13*/
-void monty_rotl(stack_t **head,  __attribute__((unused)) unsigned int count);
+void monty_rotl(stack_t **headboy,  __attribute__((unused)) unsigned int countline);
 
 /*Task 14*/
-void monty_rotr(stack_t **head, __attribute__((unused)) unsigned int count);
+void monty_rotr(stack_t **headboy, __attribute__((unused)) unsigned int countline);
 
 /*Task 15*/
-void monty_queue(stack_t **head, unsigned int line_count);
-void monty_stack(stack_t **head, unsigned int line_count);
+void monty_queue(stack_t **headboy, unsigned int countline);
+void monty_stack(stack_t **headboy, unsigned int countline);
 
 void add_queue(stack_t **head, int new);
 /*frees the stack*/
